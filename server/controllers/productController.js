6be09709a8daf5bf6def58ab9bcff24fc2f663fc
Aman1877 <mansuriaman187@gmail.com>
2023-12-20 21:43:41 +0530

    product controller and routes

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -61,28 +61,68 @@ export const updateProductController = async (req, res, nex) => {
   try {
     const { name, description, price, category, quantity, rating } = req.fields;
     const { photo } = req.files;
-    console.log(req.params);
-    // const products = await productModel.findByIdAndUpdate(
-    //   req.params.pid,
-    //   { ...req.fields, slug: slugify(name) },
-    //   { new: true }
-    // );
-    // if (photo) {
-    //   products.photo.data = fs.readFileSync(photo.path);
-    //   products.photo.contentType = photo.type;
-    // }
-    // await products.save();
-    // res.status(201).send({
-    //   success: true,
-    //   message: "Product Updated Successfully",
-    //   products,
-    // });
+    const products = await productModel.findByIdAndUpdate(
+      req.params.pid,
+      { ...req.fields, slug: slugify(name) },
+      { new: true }
+    );
+    if (photo) {
+      products.photo.data = fs.readFileSync(photo.path);
+      products.photo.contentType = photo.type;
+    }
+    await products.save();
+    res.status(201).send({
+      success: true,
+      message: "Product Updated Successfully",
+      products,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send({
       success: false,
       error,
       message: "Error while Updating product",
+    });
+  }
+};
+
+// Get product by slug (Single product) .findOne(<query>||{ slug: 'dsfdgfdg' })
+export const getSingleProductController = async (req, res, next) => {
+  // console.log({ slug: req.params.slug });
+  try {
+    const product = await productModel
+      .findOne({ slug: req.params.slug })
+      .select("-photo")
+      .populate("category");
+    res.status(200).send({
+      success: true,
+      message: "Single Product Fetched",
+      product,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Eror while getitng single product",
+      error,
+    });
+  }
+};
+
+// Get photo of product
+export const productPhotoController = async (req, res, next) => {
+  try {
+    const product = await productModel.findById(req.params.pid).select("photo");
+    if (product.photo.data) {
+      res.set("Content-type", product.photo.contentType);
+      return res.status(200).send(product.photo.data);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Erorr while getting photo",
+      error,
     });
   }
 };
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -3,6 +3,8 @@ import {
   createProductController,
   getProductController,
   updateProductController,
+  getSingleProductController,
+  productPhotoController,
 } from "../controllers/productController.js";
 import formidable from "express-formidable";
 import { addProductValidation } from "../validation/products/productValidation.js";
@@ -22,6 +24,10 @@ router.put("/update-product/:pid", formidable(), updateProductController);
 // Get products
 router.get("/get-product", getProductController);
 
-//
+// Get product by slug (Single Product)
+router.get("/get-product/:slug", getSingleProductController);
+
+// Get photo
+router.get("/product-photo/:pid", productPhotoController);
 
 export default router;
